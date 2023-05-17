a12352ee8c4aa4069abb045758e231fa5eb24b25
Kimleng <kimlenghor2006@gmail.com>
2023-05-17 11:58:34 -0500

    create file manager for images

--- SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
+++ SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
@@ -8,13 +8,70 @@
 import SwiftUI
 
 struct CoinRowView: View {
+    
+    let coin: CoinModel
+    let showHoldingsColumn: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldingsColumn {
+                middleColumn
+            }
+            
+            rightColumn
+        }
+        .font(.subheadline)
     }
 }
 
 struct CoinRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView()
+        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+            .previewLayout(.sizeThatFits)
+    }
+}
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            
+            CoinImageView(coin: coin)
+                .frame(width: 30, height: 30)
+            
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var middleColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrentWith2Decimal())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text("\(coin.currentPrice.asCurrentWith6Decimal())")
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentageString() ?? "")
+                .foregroundColor((coin.priceChangePercentage24H ?? 0) >= 0
+                                 ? Color.theme.green
+                                 : Color.theme.red)
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
     }
 }
--- SwiftfulCrypto/Extensions/PreviewProvider.swift
+++ SwiftfulCrypto/Extensions/PreviewProvider.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension PreviewProvider {
+    static var dev: DeveloperPreview {
+        return DeveloperPreview.instance
+    }
+}
+
+class DeveloperPreview {
+    
+    static let instance = DeveloperPreview()
+    private init() {}
+    
+    let homeVM = HomeViewModel()
+    
+    let coin = CoinModel(id: "bitcoin",
+                         symbol: "btc",
+                         name: "Bitcoin",
+                         image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                         currentPrice: 27043,
+                         marketCap: 523751734624,
+                         marketCapRank: 1,
+                         fullyDilutedValuation: 567676193725,
+                         totalVolume: 5988524528,
+                         high24H: 27642,
+                         low24H: 26955,
+                         priceChange24H: -405.719349311923,
+                         priceChangePercentage24H: -1.47812,
+                         marketCapChange24H: -7596814497.607422,
+                         marketCapChangePercentage24H: -1.42972,
+                         circulatingSupply: 19375106,
+                         totalSupply: 21000000,
+                         maxSupply: 21000000,
+                         ath: 69045,
+                         athChangePercentage: -60.84629,
+                         athDate: "2021-11-10T14:24:11.849Z",
+                         atl: 67.81,
+                         atlChangePercentage: 39767.26162,
+                         atlDate: "2013-07-06T00:00:00.000Z",
+                         lastUpdated: "2023-05-16T15:29:05.547Z",
+                         sparklineIn7D: SparklineIn7D(
+                            price: [
+                                27709.564810472708,
+                                27756.293586200012,
+                            ]
+                         )
+                         ,
+                         priceChangePercentage24HInCurrency: -1.4781153037128159,
+                         currentHoldings: 1.5
+                       )
+}
